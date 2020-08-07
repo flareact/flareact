4f6a1a5097280bd912f070941f4f2fbbf8c51837
Josh Larson <jplhomer@gmail.com>
2020-08-07 09:12:32 -0500

    Implement publish command

--- src/bin/flareact.js
+++ src/bin/flareact.js
@@ -11,25 +11,53 @@ const concurrently = require("concurrently");
   }
 });
 
-console.log("Starting Flareact dev server...");
+const command = process.argv[2];
 
-concurrently(
-  [
-    { command: "wrangler dev", name: "wrangler" },
+if (command === "dev") {
+  console.log("Starting Flareact dev server...");
+
+  concurrently(
+    [
+      { command: "wrangler dev", name: "wrangler" },
+      {
+        command:
+          "webpack --config node_modules/flareact/configs/webpack.client.config.js --out ./out --mode development --watch",
+        name: "client",
+      },
+    ],
     {
-      command:
-        "webpack --config node_modules/flareact/configs/webpack.client.config.js --out ./out --mode development --watch",
-      name: "client",
-    },
-  ],
-  {
-    prefix: "name",
-    killOthers: ["failure"],
-    restartTries: 0,
-  }
-).then(
-  () => {},
-  (error) => {
-    console.error(error);
-  }
-);
+      prefix: "name",
+      killOthers: ["failure"],
+      restartTries: 0,
+    }
+  ).then(
+    () => {},
+    (error) => {
+      console.error(error);
+    }
+  );
+}
+
+if (command === "publish") {
+  console.log("Publishing your Flareact project to Cloudflare...");
+
+  concurrently(
+    [
+      {
+        command:
+          "webpack --config node_modules/flareact/configs/webpack.client.config.js --out ./out --mode production && wrangler publish",
+        name: "publish",
+      },
+    ],
+    {
+      prefix: "name",
+      killOthers: ["failure"],
+      restartTries: 0,
+    }
+  ).then(
+    () => {},
+    (error) => {
+      console.error(error);
+    }
+  );
+}
