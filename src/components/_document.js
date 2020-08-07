f6d4ad8094046738094e1a176ff50fba976e9253
Josh Larson <jplhomer@gmail.com>
2020-08-07 12:29:10 -0500

    Make page responsive

--- src/components/_document.js
+++ src/components/_document.js
@@ -4,6 +4,8 @@ export default function Document({ children, initialData }) {
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width" />
+        <meta charset="utf-8" />
         <title>Flareact</title>
         {/* TODO: Find way to not show this when there are no styles */}
         <link href="/main.css" rel="stylesheet" />
