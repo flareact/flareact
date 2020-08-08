15a1b0350dd4b795eddb58a4bb91b1f8ea4ef176
Josh Larson <jplhomer@gmail.com>
2020-08-08 10:07:42 -0500

    Remove markdown loader and provide webpack-merge

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -49,17 +49,6 @@ module.exports = {
             },
           ],
         },
-        {
-          test: /\.md$/,
-          use: [
-            {
-              loader: "html-loader",
-            },
-            {
-              loader: "markdown-loader",
-            },
-          ],
-        },
       ],
     },
   },
