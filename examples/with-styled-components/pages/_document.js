adee07283c1f3d1cb492919335b4534a7c80f236
Josh Larson <jplhomer@gmail.com>
2020-11-30 15:45:33 -0600

    chore(examples): Add with-styled-components

---
+++ examples/with-styled-components/pages/_document.js
@@ -0,0 +1,30 @@
+import Document from "flareact/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getEdgeProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getEdgeProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
