1ae34c66d7912b9304b7137ea0377074164980d9
Josh Larson <jplhomer@gmail.com>
2020-12-12 16:31:01 -0600

    fix: Adjust top-scroll logic

--- src/router.js
+++ src/router.js
@@ -131,7 +131,7 @@ export function RouterProvider({
     const [, hash] = asPath.split("#");
 
     // If no hash set, scroll to top of page
-    if (hash === "") {
+    if (!hash) {
       window.scrollTo(0, 0);
     }
 
