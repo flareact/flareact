45759db7d2754667ca2e8fd9aa83136a8ceeee6d
Kasey <kaseyfkolyno@gmail.com>
2021-10-27 13:07:34 -0400

    branching to master for cloudflare worker to see

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { handleEvent } from "./worker/worker";
-// TODO: Deprecate this, as if it's used in client pages, it imports ALL THIS STUFF.
+// TODO: Deprecate this, as if it's used in client pages, it imports ALL THIS STUFF. 
 import { useRouter } from "./router";
 import { PageNotFoundError } from "./worker/pages";
 
