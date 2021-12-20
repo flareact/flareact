7f89909dc43a112db8f4f307adad8a65a07625b4
Kev <kevin.holmes@tjmorris.co.uk>
2021-12-20 12:08:10 +0000

    variable type fix

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = function ({ dev, isServer }) {
     loader: MiniCssExtractPlugin.loader,
   };
 
-  const sassOptionFiber = false;
+  let sassOptionFiber = false;
 
   // Check if we can enable fibers or not. Its not supported in node versions 16 and up.
   if (parseInt(process.versions.node.split('.')[0]) < 16) {
